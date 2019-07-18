75e086d69374cca9fdd33449c758a04b7f189a00
Nathan Hahn <nhahn@andrew.cmu.edu>
2019-07-18 12:06:07 -0400

    Typescript package fixes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 
-import AnchoredAnnotation from './annotation_types/anchored_annotation'
+import AnchoredAnnotation, {AnchoredSerializedAnnotation} from './annotation_types/anchored_annotation'
 import BaseAnnotation from './annotation_types/base_annotation'
-import ElementAnnotation from './annotation_types/element_annotation'
-import FragmentAnnotation from './annotation_types/fragment_annotation'
-import SelectionAnnotation from './annotation_types/selection_annotation'
+import ElementAnnotation, {ElementSerializedAnnotation} from './annotation_types/element_annotation'
+import FragmentAnnotation, {FragmentSerializedAnnotation} from './annotation_types/fragment_annotation'
+import SelectionAnnotation, {SelectionSerializedAnnotation} from './annotation_types/selection_annotation'
 
 import Highlight from './annotation_types/highlight'
-import ImageAnnotation from './annotation_types/image_annotation'
-import LinkAnnotation from './annotation_types/link_annotation'
+import ImageAnnotation, {ImageSerializedAnnotation} from './annotation_types/image_annotation'
+import LinkAnnotation, {LinkSerializedAnnotation} from './annotation_types/link_annotation'
 import Snippet from './annotation_types/snippet'
-import VideoAnnotation from './annotation_types/video_annotation'
+import VideoAnnotation, {VideoSerializedAnnotation} from './annotation_types/video_annotation'
 import PointAnnotation from './annotation_types/point_annotation'
 import Screenshot from './annotation_types/screenshot.js'
 
@@ -68,6 +68,7 @@ export default {
     }
 }
 
+export {AnchoredSerializedAnnotation, ElementSerializedAnnotation, FragmentSerializedAnnotation, SelectionSerializedAnnotation, ImageSerializedAnnotation, LinkSerializedAnnotation, VideoSerializedAnnotation}
 export {AnchoredAnnotation, BaseAnnotation, ElementAnnotation, FragmentAnnotation, SelectionAnnotation,
     Highlight, ImageAnnotation, LinkAnnotation, Snippet, VideoAnnotation, Screenshot, PointAnnotation,
     ClickElementSelector, DoubleTapSelector, HighlightSelector, HoverElementSelector, SnippetSelector, ElementSelector, ListAutoSelector,
